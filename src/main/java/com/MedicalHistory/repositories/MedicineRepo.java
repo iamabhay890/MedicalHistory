1346b5b1d8a86c6a6d5fa0986090b0d8763ea1da
iamabhay890 <69345405+iamabhay890@users.noreply.github.com>
2022-07-04 13:37:10 +0530

    Delete src directory

--- src/main/java/com/MedicalHistory/repositories/MedicineRepo.java
+++
@@ -1,7 +0,0 @@
-package com.MedicalHistory.repositories;
-        import com.MedicalHistory.entities.Medicine;
-        import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicineRepo extends JpaRepository<Medicine,Integer>
-{
-}
